44db192d7ba1f92726fe5f96ee3f223b503b63e1
Matthew Giovannini <shtyasek2003@mail.ru>
2022-10-27 11:05:02 +0500

    + add to playlist button + add to playlist dropdown

--- music_site/musics/static/js/music_list.js
+++ music_site/musics/static/js/music_list.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+(function music_list() {
+
+$(document).ready(function () { // play music
   let play = true;
       $("#from_pause_to_play")[0].beginElement();
   $('#pause').on('click', function(event) {
@@ -13,3 +15,39 @@ $(document).ready(function() {
     }
   });
 });
+
+let current_song = null;
+
+$(document).ready(function () { // add to playlist
+  $('.song-detail .add-song').hover(function () {
+    let song_add_dropdown = $('.song-add-dropdown');
+    let position = $(this).position();
+    song_add_dropdown.css({top: position.top + 40, left: position.left});
+    song_add_dropdown.show();
+    current_song = $(this).attr('id').split('-')[1];
+  });
+});
+
+$(document).ready(function () { //hide add to playlist
+  $('.song-add-dropdown, .song-detail .add-song').mouseleave(function () {
+    if($('.song-add-dropdown:hover').length === 0 && $('.song-detail .add-song:hover').length === 0) {
+      let song_add_dropdown = $('.song-add-dropdown');
+      song_add_dropdown.css({});
+      song_add_dropdown.hide();
+    }
+  });
+});
+
+$(document).ready(function () { //dropdown item click
+  $('.dropdown-item').click(function () {
+    let playlist_id = $(this).attr('id').split('-')[1];
+
+    if (playlist_id === 'create') {
+
+    } else {
+      $.get(`/add_song/${current_song}/${playlist_id}/`);
+    }
+  });
+});
+
+})();
