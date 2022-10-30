a2bf94e56ccb4a3ff51464e13657f42a608c0f0d
Matthew Giovannini <shtyasek2003@mail.ru>
2022-10-30 15:13:48 +0500

    + news model

--- music_site/news/admin.py
+++ music_site/news/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import News
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    fields = ('title',
+              'slug',
+              'body',
+              'image',
+              'author',
+              'published',
+              'created',
+              'updated')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created',
+                       'updated')
+    list_display = ('title',
+                    'slug',
+                    'author',
+                    'published',
+                    'created',
+                    'updated')
+    search_fields = ('title',
+                     'slug',
+                     'body',
+                     'author')
+    list_filter = ('published',
+                   'created',
+                   'updated')
+    list_editable = ('published',)
+    ordering = ('-created',
+                '-updated',
+                'title')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()
--- music_site/news/models.py
+++ music_site/news/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from auth.models import CustomUser
+
+
+class News(models.Model):
+    title = models.CharField(max_length=128,
+                             verbose_name='Заголовок')
+    slug = models.SlugField(unique=True,
+                            verbose_name='Человекопонятная ссылка')
+    body = models.TextField(blank=False,
+                            verbose_name='Текст')
+    image = models.ImageField(upload_to='media/news/%Y/%m/%d/')
+    author = models.ForeignKey(CustomUser,
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True,
+                               related_name='news',
+                               verbose_name='Автор')
+    likes = models.ManyToManyField(CustomUser,
+                                   related_name='liked_news',
+                                   verbose_name='Лайки')
+    created = models.DateTimeField(auto_now_add=True,
+                                   verbose_name='Создана')
+    updated = models.DateTimeField(auto_now=True,
+                                   verbose_name='Изменена')
+    published = models.BooleanField(default=False,
+                                    verbose_name='Опубликована')
+
+    class Meta:
+        ordering = ('-created', '-updated')
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
